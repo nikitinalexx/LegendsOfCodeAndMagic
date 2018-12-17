97614b05ef356cf31cf3900fc884dd1d4a37c1d1
nikitinalexx <nikitinalexx22@gmail.com>
2018-12-17 22:46:19 +0200

    initial players

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,12 +1,27 @@
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.codingame.game.engine.Constants;
 import com.codingame.gameengine.runner.MultiplayerGameRunner;
+import com.codingame.gameengine.runner.dto.GameResult;
 
 public class Main {
-    public static void main(String[] args) {
+    private static AtomicInteger firstWon = new AtomicInteger(0);
+    private static AtomicInteger secondWon = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        boolean runMultipleTimes = false;
+
+        if (runMultipleTimes) {
+            goMultiple();
+        } else {
+            goSingle();
+        }
+    }
+
+    private static void goSingle() {
         MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
-        
+
         Properties gameParameters = new Properties();
         // set game parameters here
         //        gameRunner.setSeed(1279960l);
@@ -16,15 +31,58 @@ public class Main {
         //        gameParameters.setProperty("predefinedDraftIds", "91 92 93,94 95 96,97 98 99,100 101 102,103 104 105,106 107 108,109 110 111,112 113 114,115 116 117,118 119 120,121 122 123,124 125 126,127 128 129,130 131 132,133 134 135,136 137 138,139 140 141,142 143 144,145 146 147,148 149 150,151 152 153,154 155 156,157 158 159,160 160 160,160 160 160,160 160 160,160 160 160,160 160 160,160 160 160,160 160 160");
         gameRunner.setGameParameters(gameParameters);
 
-        gameRunner.addAgent(PlayerEmpty.class);
-        gameRunner.addAgent(PlayerEmpty.class);
+        gameRunner.addAgent(current.Player.class);
+        gameRunner.addAgent(previous.Player.class);
 
         Constants.VERBOSE_LEVEL = 2;
-        
+
         //set ruleset here
         System.setProperty("league.level", "4");
-        
+
         gameRunner.start();
+    }
+
+    private static void goMultiple() throws InterruptedException {
+        for (int t = 0; t < 1; t++) {
+            Thread thread = new Thread(() -> {
+                for (int i = 0; i < 50; i++) {
+                    MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
+
+                    Properties gameParameters = new Properties();
+                    // set game parameters here
+                    //        gameRunner.setSeed(1279960l);
+                    //        gameParameters.setProperty("draftChoicesSeed", "-5113144502819146988");
+                    //        gameParameters.setProperty("shufflePlayer0Seed", "127");
+                    //        gameParameters.setProperty("shufflePlayer1Seed", "333");
+                    //        gameParameters.setProperty("predefinedDraftIds", "91 92 93,94 95 96,97 98 99,100 101 102,103 104 105,106 107 108,109 110 111,112 113 114,115 116 117,118 119 120,121 122 123,124 125 126,127 128 129,130 131 132,133 134 135,136 137 138,139 140 141,142 143 144,145 146 147,148 149 150,151 152 153,154 155 156,157 158 159,160 160 160,160 160 160,160 160 160,160 160 160,160 160 160,160 160 160,160 160 160");
+                    gameRunner.setGameParameters(gameParameters);
+
+                    gameRunner.addAgent(current.Player.class);
+                    gameRunner.addAgent(previous.Player.class);
+
+                    Constants.VERBOSE_LEVEL = 0;
+
+                    //set ruleset here
+                    System.setProperty("league.level", "4");
+
+
+                    GameResult simulate = gameRunner.simulate();
+                    System.out.println("Simulating " + simulate.scores);
+                    if (simulate.scores.get(0) == 1) {
+                        firstWon.addAndGet(1);
+                    }
+                    if (simulate.scores.get(1) == 1) {
+                        secondWon.addAndGet(1);
+                    }
+                }
+            });
+            thread.start();
+        }
+        while (true) {
+            Thread.sleep(1000);
+            System.out.println("First " + firstWon.intValue() + ". Second " + secondWon.intValue());
+        }
+
     }
 
 }
---
+++ src/test/java/previous/Player.java
@@ -0,0 +1,171 @@
+package previous;
+
+import com.alex.nikitin.Card;
+import com.alex.nikitin.CardType;
+
+import java.util.*;
+
+/**
+ * AI description
+ * Draft phase:
+ * - always pick the first card
+ * Game phase:
+ * - do nothing (outputs single ';')
+ */
+public class Player {
+    public static final int PLAYER_ID = -1;
+    private static List<Card> myMobsOnBoard = new ArrayList<>();
+    private static List<Card> opponentMobsOnBoard = new ArrayList<>();
+    private static List<Card> greenCards = new ArrayList<>();
+    private static PriorityQueue<Card> handCards = new PriorityQueue<>(Comparator.comparingInt((Card card) -> card.cost).reversed());
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        // game loop
+        while (true) {
+            myMobsOnBoard.clear();
+            opponentMobsOnBoard.clear();
+            greenCards.clear();
+            handCards.clear();
+            String result = "";
+            int myMana = 0;
+            for (int i = 0; i < 2; i++) {
+                int playerHealth = in.nextInt();
+                int playerMana = in.nextInt();
+                int playerDeck = in.nextInt();
+                int playerRune = in.nextInt();
+                int playerDraw = in.nextInt();
+                if (i == 0) {
+                    myMana = playerMana;
+                }
+            }
+            int opponentHand = in.nextInt();
+            int opponentActions = in.nextInt();
+            if (in.hasNextLine()) {
+                in.nextLine();
+            }
+            for (int i = 0; i < opponentActions; i++) {
+                String cardNumberAndAction = in.nextLine();
+            }
+
+            int cardCount = in.nextInt();
+            for (int i = 0; i < cardCount; i++) {
+                Card card = new Card(
+                        in.nextInt(), in.nextInt(),
+                        in.nextInt(), in.nextInt(),
+                        in.nextInt(), in.nextInt(),
+                        in.nextInt(), in.next(),
+                        in.nextInt(), in.nextInt(), in.nextInt()
+                );
+                if (card.cardType == CardType.CREATURE.value) {
+                    addCreatureCard(card);
+                }
+                if (card.cardType == CardType.GREEN.value) {
+                    addGreenCard(card);
+                }
+
+            }
+
+            result += summonCreatures(myMana);
+            result += applyGreenCards();
+            result += attackCreatures();
+
+
+
+
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+            if (!result.equals("")) {
+                System.err.println(result);
+                System.out.println(result);
+            } else {
+                System.out.println("PASS");
+            }
+        }
+    }
+
+    private static void addCreatureCard(Card card) {
+        if (card.location == 0) {
+            handCards.add(card);
+        }
+
+        if (card.location == 1) {
+            myMobsOnBoard.add(card);
+        }
+        if (card.location == -1) {
+            opponentMobsOnBoard.add(card);
+        }
+    }
+
+    private static String applyGreenCards() {
+        String result = "";
+        for (int i = 0; i < greenCards.size(); i++) {
+            Card greenCard = greenCards.get(i);
+            for (int j = 0; j < myMobsOnBoard.size(); j++) {
+                if (!myMobsOnBoard.get(j).abilities.contains("G")) {
+                    myMobsOnBoard.get(j).abilities += "G";
+                    result += useItem(greenCard.instanceId, myMobsOnBoard.get(j).instanceId);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    private static void addGreenCard(Card card) {
+        if (card.location == 0) {
+            greenCards.add(card);
+        }
+    }
+
+    private static String summonCreatures(int myMana) {
+        String result = "";
+        Card card;
+        while ((card = handCards.poll()) != null) {
+            if (card.cost < myMana) {
+                result += summon(card.instanceId);
+                myMana -= card.cost;
+                myMobsOnBoard.add(card);
+            }
+        }
+
+        Collections.sort(myMobsOnBoard, Comparator.comparingInt((Card cardToShuffle) -> cardToShuffle.attack).reversed());
+        return result;
+    }
+
+    private static String attackCreatures() {
+        String result = "";
+        for (int i = 0; i < myMobsOnBoard.size(); i++) {
+            Card myMob = myMobsOnBoard.get(i);
+            int maxLess = PLAYER_ID;
+            int maxHealth = 0;
+            for (int j = 0; j < opponentMobsOnBoard.size(); j++) {
+                Card enemyBoard = opponentMobsOnBoard.get(j);
+                if (enemyBoard.defense <= myMob.attack && enemyBoard.defense > maxHealth && (myMob.defense > enemyBoard.attack || enemyBoard.attack / myMob.defense > 1) && !enemyBoard.abilities.contains("G")
+                        || enemyBoard.abilities.contains("G")) {
+                    maxHealth = enemyBoard.defense;
+                    maxLess = enemyBoard.instanceId;
+                }
+            }
+            result += attack(myMob.instanceId, maxLess);
+            result += attack(myMob.instanceId, PLAYER_ID);//in case the creature to attack already dead.
+        }
+        return result;
+    }
+
+    private static String attack(int attacker, int defender) {
+        return String.format("ATTACK %s %s;", attacker, defender);
+    }
+
+    private static String summon(int whoToSummon) {
+        return String.format("SUMMON %s;", whoToSummon);
+    }
+
+    private static String useItem(int card, int creature) {
+        return String.format("USE %s %s;", card, creature);
+    }
+
+
+
+}
